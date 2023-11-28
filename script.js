2a8a7c11549f71a397aeee22f48afa7f76a84b08
Omfa94 <147593282+Omfa94@users.noreply.github.com>
2023-11-28 12:11:19 +0100

    ändring

---
+++ script.js
@@ -0,0 +1,10 @@
+const checkbox=document.getElementById("divStyle");
+
+const textfield=document.querySelectorAll(".textfield");
+
+const button=document.querySelector(".button");
+
+const emptyDiv=document.getElementsByClassName("empty_div");
+
+function handleBlure(e){console.log("eventlyssnaren handleBlur,eventobjektet:",e);
+};
